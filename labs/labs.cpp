f841b21308153493a10596180f7565fc6a6a8ef5
Georgy <georgy.tertychniy@yandex.ru>
2021-11-11 19:25:41 +0300

    aboba

--- labs/labs.cpp
+++ labs/labs.cpp
@@ -1,6 +1,4 @@
-﻿// labs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#define _USE_MATH_DEFINES
+﻿#define _USE_MATH_DEFINES
 #include <iostream>
 #include <string>
 #include<numeric>
@@ -20,206 +18,74 @@ void lab15(std::pair<double, double> segment, double step)
 	}
 }
 
-//16 вариант 23
-class matrix
-{
-private:
-	float** _matrix;
-public:
-	float min_in_upper_triangle_matrix()
-	{
-		float min = _matrix[0][0];
-		for (int i = 0; i < SIDE; i++)
-			for (int j = i; j < SIDE; j++)
-			{
-				min = min > _matrix[i][j] ? _matrix[i][j] : min;
-			}
-		return min;
-	}
-
-	float find_multiple(float lessNumber)
-	{
-		bool findAnyNumber = false;
-		float multiple = 1;
-		for (int i = 0; i < SIDE; i++)
-			for (int j = 0; j <= i; j++)
-			{
-				if (_matrix[i][j] < lessNumber)
-				{
-					multiple *= _matrix[i][j];
-					findAnyNumber = true;
-				}
-			}
-		if (!findAnyNumber)
-			throw std::string("all numbers are greater than a given number");
-		return  multiple;
-	}
-
-	const int SIDE;
-
-	matrix(const int side, float** m) :SIDE(side)
-	{
-		_matrix = new float* [side];
-		for (int i = 0; i < side; i++)
-		{
-			_matrix[i] = new float[side];
-			for (int a = 0; a < side; a++)
-			{
-				_matrix[i][a] = m[i][a];
-			}
-		}
-	}
-	void lab16()
-	{
-		for (int i = 0; i < SIDE; i++)
-		{
-			for (int j = 0; j < SIDE; j++)
-			{
-				std::cout << _matrix[i][j] << "\t";
-			}
-			std::cout << "\n";
-		}
-		float min = min_in_upper_triangle_matrix();
-		std::cout << "min: " << min << "\n";
-		try
-		{
-			std::cout << "multiple = " << find_multiple(min_in_upper_triangle_matrix());
-		}
-		catch (std::string ex)
-		{
-			std::cout << ex;
-		}
-	}
-	~matrix()
-	{
-		for (int i = 0; i < SIDE; i++)
-		{
-			delete[] _matrix[i];
-		}
-		delete[] _matrix;
-	}
-};
-
-float** matrix_console_input(int sideElements)
-{
-	std::regex reg = std::regex(R"(([-]?\d+\d*([.]\d+)*))");
-	float** arr = new float* [sideElements];
-	for (int i = 0; i < sideElements; i++)
-	{
-		std::string str;
-		std::cin.sync();
-		std::getline(std::cin, str);
-		std::vector<std::string> matches{
-			std::sregex_token_iterator{str.begin(), str.end(), reg, 0},
-			std::sregex_token_iterator() };
-
-		if (matches.size() != sideElements)
-			throw "too many elements";
-
-		float* array = new float[sideElements];
-		for (int j = 0; j < sideElements; j++)
-		{
-			array[j] = std::atof(matches[j].c_str());
-		}
-		arr[i] = array;
-	}
-	return  arr;
-}
-
-class equationSolution
-{
-public:
-	static constexpr float START_X = -0.17f;
-
-	static  float get_function_result(float x)
-	{
-		return exp(-3 * x) - sin(x) - 1.5;
-	}
-	static float derivative(float x)
-	{
-		return (3 + exp(3 * x) * cos(x)) / exp(3 * x);
-	}
-	static float g_x(float x)
-	{
-		return x - derivative(1.9) * get_function_result(x);
-	}
-	static float iterative_method(float x, float e, int maxIteration = 1000000)
-	{
-		float result = g_x(x);
-		int iter = 0;
-
-		while (abs(x - result) > e && iter < maxIteration)
-		{
-			x = result;
-			result = g_x(x);
-			iter++;
-		}
-		if (maxIteration != iter)
-			return x;
-		throw std::string("infinity loop");
-	}
-
-	static float binary_method(float left, float right, float e)
-	{
-		if (get_function_result(left) * get_function_result(right) >= 0)
-			throw std::string("incorrect");
-		float halfSum = get_function_result((left + right) / 2);
-		if (halfSum / get_function_result(left) > 0)
-		{
-			left = (left + right) / 2;
-		}
-		else right = (left + right) / 2;
-
-		if (abs(right - left) > e)
-			return binary_method(left, right, e);
-		// return get_function_result((left + right) / 2);
-		return (left + right) / 2;
-	}
-};
-
 namespace lab18
 {
-
+	template<typename T>
+	void input_arr(T* arr, int len, std::istream& stream)
+	{
+		for (int i = 0; i < len; i++)
+		{
+			T num;
+			stream >> num;
+			arr[i] = num;
+		}
+	}
+	template<typename T>
+	void output(T* arr, int len, std::ostream& stream)
+	{
+		for (int i = 0; i < len; i++)
+			stream << arr[i] << "\n";
+	}
 	typedef int (*compare_function_type) (const void*, const void*);
 
-	template<typename T>
-	T find_minimum(T* startIter, T* endIter, compare_function_type predic)
+	void swap_byte(char* first_pointer, char* second_pointer) 
 	{
-		T max = *startIter;
-		while (startIter != endIter - 1)
+		char temp = *first_pointer;
+		*first_pointer = *second_pointer;
+		*second_pointer = temp;
+	}
+	void swap(void* first, void* second, int width)
+	{
+		for (int byte = 0; byte < width; ++byte) 
 		{
-			if (predic(&max, startIter + 1) == 1)
+			swap_byte((char*)first + byte,
+				(char*)second + byte);
+		}
+
+	}
+
+	template<typename T>
+	int find_index(T* startIter, T* endIter, compare_function_type comparator)
+	{
+		T actualNum = *startIter;
+		int actualIndex = 0;
+		int index = 1;
+		for(; startIter != endIter -1; index++)
+		{
+			if (comparator(&actualNum, startIter + 1) == -1)
 			{
-				max = *(startIter + 1);
+				actualNum = *(startIter + 1);
+				actualIndex = index;
 			}
 			startIter++;
 		}
-		return max;
+		return actualIndex;
 	}
 	template<typename T>
-	void selection_sort(T* startIter, T* endIter, compare_function_type predic)
+	void selection_sort(T* startIter, T* endIter, compare_function_type comparator)
 	{
-		if (startIter == endIter)
-			return;
 		T* nowIterate = startIter;
-		for (int offset = 1; startIter + offset != endIter; offset++)
+		for (int offset = 0; startIter + offset != endIter ; offset++)
 		{
-			T* min = nowIterate;
-			while (nowIterate != endIter)
-			{
-				if (predic(min, nowIterate) == -1)
-				{
-					min = nowIterate;
-				}
-				++nowIterate;
-			}
-			std::swap(*min, *(startIter + offset - 1));
-			nowIterate = startIter + offset;
+			int index = find_index(nowIterate, endIter, comparator);
+			//swap((void*) nowIterate[index], (void*)(nowIterate), sizeof(nowIterate));
+			std::swap(*(nowIterate+index), *nowIterate);
+			nowIterate = startIter + offset+1;
 		}
 	}
 
 	template<typename T>
-	int Compare(const void* firstP, const void* secondP)
+	int compare(const void* firstP, const void* secondP)
 	{
 		T first = *static_cast<const T*>(firstP);
 		T second = *static_cast<const T*>(secondP);
@@ -228,6 +94,11 @@ namespace lab18
 		if (second > first)
 			return 1;
 		return 0;
+	}
+	template<typename T>
+	int reverce(const void* firstP, const void* secondP)
+	{
+		return -1 * compare<T>(firstP, secondP);
 	}
 }
 
@@ -264,10 +135,17 @@ public:
 		return recursive(index);
 	}
 };
+
+namespace sorts
+{
+
+}
 struct complex
 {
 	double first;
 	double second;
+	static unsigned long long compare_count;
+	static unsigned long long copy_counter;
 	complex(double first, double second)
 	{
 		this->first = first;
@@ -281,23 +159,50 @@ struct complex
 	{
 		return (first + second) > (c.first + c.second);
 	}
-};
 
-void BubbleSort(complex* startP, complex* endP)
-{
-}
+	static void BubbleSort(complex* startP, complex* endP)
+	{
+		complex* iterP = startP;
+		complex* startIteration = startP;
+		for (int i = 0; startP != endP - 1; startP++, i++)
+		{
+			for (; iterP != endP - 1 - i; iterP++)
+			{
+				if (*iterP < *(iterP + 1))
+				{
+					std::swap(*iterP, *(iterP + 1));
+				}
+			}
+			iterP = startIteration;
+		}
+	}
+};
 int main()
 {
-	/*lab18::compare_function_type predic = lab18::Compare<int>;
-	int arr[]{ 1,2,3,4,5,1 };
+	//18 лаба
+	lab18::compare_function_type predic = lab18::compare<int>;
+	int len;
+	std::cin >> len;
+	float *arr = new float[len];
+	lab18::input_arr(arr, len, std::cin);
+	int index = lab18::find_index(arr, arr + len, predic);
+	std::cout <<"index: "<< index<<" result: " <<arr[index] << std::endl;
+	lab18::selection_sort(arr, arr+len, predic);
+	lab18::output(arr, len, std::cout);
+	delete[] arr;
+	arr = nullptr;
 
-	std::cout << lab18::find_minimum(std::begin(arr), std::end(arr), predic) << std::endl;
-	lab18::selection_sort(std::begin(arr), std::end(arr), predic);
-	for (auto i : arr)
-	{
-		std::cout << i << std::endl;
-	}*/
 
-	fibonacci f = fibonacci();
-	f[1];
+	//19 лаба
+	//fibonacci f = fibonacci();
+	//f[1];
+
+
+	//20 лаба
+	//complex arr[] = { complex(1,3), complex(3,3), complex(1,1), complex(5,5), complex(0,0) };
+	//complex::BubbleSort(std::begin(arr), std::end(arr));	//O(n^2)
+	//for (auto a : arr)
+	//{
+	//	std::wcout << a.first << " " << a.second << "\n";
+	//}
 }
--- labs/oldLabClass.cpp
+++ labs/oldLabClass.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include<regex>
 
     //вариант 2, лаба 4
     void lab4()
@@ -320,3 +321,160 @@
             std::cout << "Prime";
         else std::cout << "not a prime number";
     }
+
+    //16 вариант 23
+    class matrix
+    {
+    private:
+        float** _matrix;
+    public:
+        float min_in_upper_triangle_matrix()
+        {
+            float min = _matrix[0][0];
+            for (int i = 0; i < SIDE; i++)
+                for (int j = i; j < SIDE; j++)
+                {
+                    min = min > _matrix[i][j] ? _matrix[i][j] : min;
+                }
+            return min;
+        }
+
+        float find_multiple(float lessNumber)
+        {
+            bool findAnyNumber = false;
+            float multiple = 1;
+            for (int i = 0; i < SIDE; i++)
+                for (int j = 0; j <= i; j++)
+                {
+                    if (_matrix[i][j] < lessNumber)
+                    {
+                        multiple *= _matrix[i][j];
+                        findAnyNumber = true;
+                    }
+                }
+            if (!findAnyNumber)
+                throw std::string("all numbers are greater than a given number");
+            return  multiple;
+        }
+
+        const int SIDE;
+
+        matrix(const int side, float** m) :SIDE(side)
+        {
+            _matrix = new float* [side];
+            for (int i = 0; i < side; i++)
+            {
+                _matrix[i] = new float[side];
+                for (int a = 0; a < side; a++)
+                {
+                    _matrix[i][a] = m[i][a];
+                }
+            }
+        }
+        void lab16()
+        {
+            for (int i = 0; i < SIDE; i++)
+            {
+                for (int j = 0; j < SIDE; j++)
+                {
+                    std::cout << _matrix[i][j] << "\t";
+                }
+                std::cout << "\n";
+            }
+            float min = min_in_upper_triangle_matrix();
+            std::cout << "min: " << min << "\n";
+            try
+            {
+                std::cout << "multiple = " << find_multiple(min_in_upper_triangle_matrix());
+            }
+            catch (std::string ex)
+            {
+                std::cout << ex;
+            }
+        }
+        ~matrix()
+        {
+            for (int i = 0; i < SIDE; i++)
+            {
+                delete[] _matrix[i];
+            }
+            delete[] _matrix;
+        }
+    };
+
+    float** matrix_console_input(int sideElements)
+    {
+        std::regex reg = std::regex(R"(([-]?\d+\d*([.]\d+)*))");
+        float** arr = new float* [sideElements];
+        for (int i = 0; i < sideElements; i++)
+        {
+            std::string str;
+            std::cin.sync();
+            std::getline(std::cin, str);
+            std::vector<std::string> matches{
+                std::sregex_token_iterator{str.begin(), str.end(), reg, 0},
+                std::sregex_token_iterator() };
+
+            if (matches.size() != sideElements)
+                throw "too many elements";
+
+            float* array = new float[sideElements];
+            for (int j = 0; j < sideElements; j++)
+            {
+                array[j] = std::atof(matches[j].c_str());
+            }
+            arr[i] = array;
+        }
+        return  arr;
+    }
+
+    class equationSolution
+    {
+    public:
+        static constexpr float START_X = -0.17f;
+
+        static  float get_function_result(float x)
+        {
+            return exp(-3 * x) - sin(x) - 1.5;
+        }
+        static float derivative(float x)
+        {
+            return (3 + exp(3 * x) * cos(x)) / exp(3 * x);
+        }
+        static float g_x(float x)
+        {
+            return x - derivative(1.9) * get_function_result(x);
+        }
+        static float iterative_method(float x, float e, int maxIteration = 1000000)
+        {
+            float result = g_x(x);
+            int iter = 0;
+
+            while (abs(x - result) > e && iter < maxIteration)
+            {
+                x = result;
+                result = g_x(x);
+                iter++;
+            }
+            if (maxIteration != iter)
+                return x;
+            throw std::string("infinity loop");
+        }
+
+        static float binary_method(float left, float right, float e)
+        {
+            if (get_function_result(left) * get_function_result(right) >= 0)
+                throw std::string("incorrect");
+            float halfSum = get_function_result((left + right) / 2);
+            if (halfSum / get_function_result(left) > 0)
+            {
+                left = (left + right) / 2;
+            }
+            else right = (left + right) / 2;
+
+            if (abs(right - left) > e)
+                return binary_method(left, right, e);
+            // return get_function_result((left + right) / 2);
+            return (left + right) / 2;
+        }
+    };
